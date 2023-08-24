a65b5588e59c386f23e1f5d2aaee6d844af0321f
Michelly Moreira <michelly.daiana@hotmail.com>
2023-08-24 19:24:12 -0300

    Hotel model

--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -3,5 +3,14 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 public class Hotel {
+  [Key]
+  public int HotelId {get; set;}
+  public string? Name {get; set;}
+  public string? Address {get; set;}
 
+  [ForeignKey("CityId")]
+  public int CityId {get; set;}
+  public virtual City? City {get; set;}
+
+  public ICollection<Room>? Rooms {get; set;}
 }
