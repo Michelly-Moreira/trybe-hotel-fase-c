ea03635e01ef7cf7140a5b5812072a65a09d7c4f
Michelly Moreira <michelly.daiana@hotmail.com>
2023-08-24 19:25:28 -0300

    Room model

--- src/TrybeHotel/Models/Room.cs
+++ src/TrybeHotel/Models/Room.cs
@@ -3,5 +3,15 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 public class Room {
+  [Key]
+  public int RoomId {get; set;}
+  public string? Name {get; set;}
+  public int Capacity {get; set;}
+  public string? Image {get; set;}
 
+  [ForeignKey("HotelId")]
+  public int HotelId {get; set;}
+  public virtual Hotel? Hotel {get; set;}
+
+  public ICollection<Booking>? Bookings {get; set;}
 }
