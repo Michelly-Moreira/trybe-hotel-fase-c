c77fdab626e09eb055111a2e9ec0e6f20ad118b5
Michelly Moreira <michelly.daiana@hotmail.com>
2023-08-24 19:23:17 -0300

    Booking model

--- src/TrybeHotel/Models/Booking.cs
+++ src/TrybeHotel/Models/Booking.cs
@@ -3,5 +3,17 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 public class Booking {
+  [Key]
+  public int BookingId {get; set;}
+  public DateTime CheckIn {get; set;}
+  public DateTime CheckOut {get; set;}
+  public int GuestQuant {get; set;}
 
+  [ForeignKey("UserId")]
+  public int UserId {get; set;}
+  public virtual User? User {get; set;}
+
+  [ForeignKey("RoomId")]
+  public int RoomId {get; set;}
+  public virtual Room? Room {get; set;}
 }
