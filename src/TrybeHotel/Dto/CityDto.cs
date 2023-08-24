5d69de1a9593018fe64f7ac40f3190da357ce9e9
Michelly Moreira <michelly.daiana@hotmail.com>
2023-08-24 19:17:28 -0300

    CityDTO e HotelDTO

--- src/TrybeHotel/Dto/CityDto.cs
+++ src/TrybeHotel/Dto/CityDto.cs
@@ -3,5 +3,7 @@ namespace TrybeHotel.Dto {
     //2. Refatore o endpoint POST /city
     //4. Refatore o endpoint GET /city
      public class CityDto {
+        public int CityId {get; set;}
+        public string? Name {get; set;}
     }
 }
