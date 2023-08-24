2eb19c92611e17e9bc291aabbcf9039118e5e579
Michelly Moreira <michelly.daiana@hotmail.com>
2023-08-24 19:26:12 -0300

    User Model

--- src/TrybeHotel/Models/User.cs
+++ src/TrybeHotel/Models/User.cs
@@ -3,5 +3,11 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 public class User {
-
+  [Key]
+  public int UserId {get; set;}
+  public string? Name {get; set;}
+  public string? Email {get; set;}
+  public string? Password {get; set;}
+  public string? UserType {get; set;}
+  public ICollection<Booking>? Bookings {get; set;}
 }
