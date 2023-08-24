73412cb86eae8b26ca9a20f22344c9c7530dbc56
Michelly Moreira <michelly.daiana@hotmail.com>
2023-08-24 19:34:57 -0300

    HotelRepository

--- src/TrybeHotel/Repository/HotelRepository.cs
+++ src/TrybeHotel/Repository/HotelRepository.cs
@@ -14,13 +14,42 @@ namespace TrybeHotel.Repository
         //  5. Refatore o endpoint GET /hotel
         public IEnumerable<HotelDto> GetHotels()
         {
-            throw new NotImplementedException();
+            var listHotels = _context.Hotels;
+            var listCities = _context.Cities;
+
+            var allHotels = from hotel in listHotels
+            from city in listCities
+            where hotel.CityId == city.CityId
+            select new HotelDto {
+                HotelId = hotel.HotelId,
+                Name = hotel.Name,
+                Address = hotel.Address,
+                CityId = hotel.CityId,
+                CityName = city.Name,
+            };
+            return allHotels;
         }
 
         // 6. Refatore o endpoint POST /hotel
         public HotelDto AddHotel(Hotel hotel)
         {
-           throw new NotImplementedException();
+           var listCities = _context.Cities;
+            var listHotels = _context.Hotels;
+            listHotels.Add(hotel);
+            _context.SaveChanges();
+            
+            var newHotel = from oneHotel in listHotels
+            join city in listCities on oneHotel.CityId equals city.CityId
+            where oneHotel.HotelId == hotel.HotelId
+            select new HotelDto
+            {
+                HotelId = hotel.HotelId,
+                Name = hotel.Name,
+                Address = hotel.Address,
+                CityId = city.CityId,
+                CityName = city.Name
+            };
+            return newHotel.Last();
         }
     }
 }
