d0532c9beb63983fa68ad714d4087de1eb8bff94
Michelly Moreira <michelly.daiana@hotmail.com>
2023-08-24 21:50:44 -0300

    UserRepository

--- src/TrybeHotel/Repository/UserRepository.cs
+++ src/TrybeHotel/Repository/UserRepository.cs
@@ -17,21 +17,77 @@ namespace TrybeHotel.Repository
 
         public UserDto Login(LoginDto login)
         {
-            throw new NotImplementedException();
+            var allUsers = _context.Users;
+            var existingLogin = from user in allUsers
+            where user.Email == login.Email
+            where user.Password == login.Password
+            select new UserDto
+            {
+                UserId = user.UserId,
+                Name = user.Name,
+                Email = user.Email,
+                UserType = user.UserType,
+            };
+            switch(existingLogin.FirstOrDefault())
+            {
+                case null:
+                return null;
+                default:return existingLogin.First();
+            }
         }
         public UserDto Add(UserDtoInsert user)
         {
-            throw new NotImplementedException();
+            var allUsers = _context.Users;
+            
+            var newClient = new User
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+                UserType = "client",
+            };
+            allUsers.Add(newClient);
+            _context.SaveChanges();
+
+            return new UserDto
+            {
+                UserId = newClient.UserId,
+                Name = newClient.Name,
+                Email = newClient.Email,
+                UserType = newClient.UserType,
+            };
         }
 
         public UserDto GetUserByEmail(string userEmail)
         {
-             throw new NotImplementedException();
+             var allUsers = _context.Users;
+            var registeredEmail = from user in allUsers
+            where user.Email == userEmail
+            select new UserDto
+            {
+                Email = user.Email,
+            };
+            switch(registeredEmail.Count())
+            {
+                case 0:
+                return null;
+                default:return registeredEmail.First();
+            } 
         }
 
         public IEnumerable<UserDto> GetUsers()
         {
-            throw new NotImplementedException();
+            var allUsers = _context.Users;
+
+           var getAllUsers = from user in allUsers
+           select new UserDto
+           {
+            UserId = user.UserId,
+            Name = user.Name,
+            Email = user.Email,
+            UserType = user.UserType,
+           };
+           return getAllUsers;
         }
 
     }
