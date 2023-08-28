3c95e7cd556c12d4ca4795ba08d372f40adbe484
Michelly Moreira <michelly.daiana@hotmail.com>
2023-08-28 14:55:00 -0300

    requisição assíncrona na controller

--- src/TrybeHotel/Controllers/GeoController.cs
+++ src/TrybeHotel/Controllers/GeoController.cs
@@ -35,7 +35,7 @@ namespace TrybeHotel.Controllers
         [Route("address")]
         public async Task<IActionResult> GetHotelsByLocation([FromBody] GeoDto address)
         {
-            var hotelsByDistance = _geoService.GetHotelsByGeo(address, _repository);
+            var hotelsByDistance = await _geoService.GetHotelsByGeo(address, _repository);
             return Ok(hotelsByDistance);
         }
     }
